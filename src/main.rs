9a2f2c481289c750dba9f173ce1ace785a30e4b5
imlonghao <git@imlonghao.com>
2021-05-12 23:48:46 +0800

    add vigil reporter

--- src/main.rs
+++ src/main.rs
@@ -4,12 +4,32 @@ use reqwest;
 use std::env;
 use std::thread;
 use std::time::Duration;
+use vigil_reporter::Reporter;
 
 mod influxdb;
 mod toshl;
 
 fn main() {
-    let (token, dburl, dbuser, dbpass) = parse_env();
+    let (
+        token,
+        dburl,
+        dbuser,
+        dbpass,
+        vigil_host,
+        vigil_secret,
+        vigil_probe_id,
+        vigil_node_id,
+        vigil_replica_id,
+    ) = parse_env();
+    if vigil_host != "" {
+        vigil_worker(
+            &vigil_host,
+            &vigil_secret,
+            &vigil_probe_id,
+            &vigil_node_id,
+            &vigil_replica_id,
+        );
+    }
     let db = influxdb::InfluxDB::new(dburl, dbuser, dbpass);
     let mut rng = rand::thread_rng();
     loop {
@@ -35,12 +55,37 @@ fn main() {
     }
 }
 
-fn parse_env() -> (String, String, String, String) {
+fn parse_env() -> (
+    String,
+    String,
+    String,
+    String,
+    String,
+    String,
+    String,
+    String,
+    String,
+) {
     let token = env::var("TOKEN").expect("TOKEN is empty");
     let dburl = env::var("DBURL").expect("DBURL is empty");
     let dbuser = env::var("DBUSER").expect("DBUSER is empty");
     let dbpass = env::var("DBPASS").expect("DBPASS is empty");
-    (token, dburl, dbuser, dbpass)
+    let vigil_host = env::var("VIGIL_HOST").unwrap_or("".to_string());
+    let vigil_secret = env::var("VIGIL_SECRET").unwrap_or("".to_string());
+    let vigil_probe_id = env::var("VIGIL_PROBE_ID").unwrap_or("".to_string());
+    let vigil_node_id = env::var("VIGIL_NODE_ID").unwrap_or("".to_string());
+    let vigil_replica_id = env::var("VIGIL_REPLICA_ID").unwrap_or("".to_string());
+    (
+        token,
+        dburl,
+        dbuser,
+        dbpass,
+        vigil_host,
+        vigil_secret,
+        vigil_probe_id,
+        vigil_node_id,
+        vigil_replica_id,
+    )
 }
 
 fn fetch_toshl(token: &String) -> Result<f64, reqwest::Error> {
@@ -55,4 +100,19 @@ fn fetch_toshl(token: &String) -> Result<f64, reqwest::Error> {
         balance += account.balance / account.currency.rate;
     }
     Ok(balance)
+}
+
+fn vigil_worker(
+    vigil_host: &String,
+    vigil_secret: &String,
+    vigil_probe_id: &String,
+    vigil_node_id: &String,
+    vigil_replica_id: &String,
+) {
+    let reporter = Reporter::new(vigil_host, vigil_secret)
+        .probe_id(vigil_probe_id) // Probe ID containing the parent Node for Replica
+        .node_id(vigil_node_id) // Node ID containing Replica
+        .replica_id(vigil_replica_id) // Unique Replica ID for instance (ie. your IP on the LAN)
+        .build();
+    reporter.run().expect("failed to start vigil reporter");
 }
